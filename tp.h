28c2c0d0e39c89a1eb5b2366c7ebd77e710442a4
dendrobiumz <page19920610@gmail.com>
2025-03-24 23:21:17 +0800

    [Update] implement struct thread pool and struct thread_task

--- tp.h
+++ tp.h
@@ -6,21 +6,24 @@
 
 #define THREAD_NBRS (8)
 
-struct thread_task {
+typedef struct thread_task {
     void *args;
     void* (*task_func)(void*);
-};
+    struct list_head node;
+} thread_task_t;
 
 
-struct thread_pool {
+
+typedef struct thread_pool {
     pthread_t threads[THREAD_NBRS];
-    size_t thread_task_nbrs;
-    
-};
+    lfq_t *worker_queues[THREAD_NBRS];
+    size_t task_nbrs;
+    int (*push_work)(struct thread_pool *self, struct thread_task *task);
+} tp_t;
 
 
-struct thread_pool* init_thread_pool();
-
+tp_t* new_thread_pool();
+void init_thread_pool(tp_t* tp);
 
 
 #endif
